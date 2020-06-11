abe7aa43bca1be633fed52f1df82e1730875e456
t2ntruong811 <t2ntruong811@gmail.com>
2020-06-12 04:26:21 +0700

    Update main.js

--- main.js
+++ main.js
@@ -9,6 +9,9 @@ const PORT = process.env.PORT || 3000
 
 app.use(express.json());
 
+app.get('/', (req, res) => {
+    res.send('Hellooo');
+})
 app.use('/api/customers', require('./routes/customers-route'))
 app.use('/api/type_transactions', require('./routes/type_transactions-route'))
 
