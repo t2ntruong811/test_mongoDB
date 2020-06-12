add1297e967e2693723bd293c7ecde57a36667a8
t2ntruong811 <t2ntruong811@gmail.com>
2020-06-12 13:03:34 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,21 +1,9 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const connectDB = require('./utils/connection')
-const mongoose = require('mongoose')
 const app = express()
 
-// connectDB()
-
-console.log('uri', process.env.mongodb_uri);
-
-mongoose.connect(process.env.mongodb_uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-
-mongoose.connection.on('connected', () => {
-    console.log('Connected!');
-})
+connectDB()
 
 const PORT = process.env.PORT || 3000
 
