11c98e6d472f051807b0f07a6954be8bb30be622
t2ntruong811 <t2ntruong811@gmail.com>
2020-06-12 12:44:48 +0700

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const app = express()
 
 // connectDB()
 
-console.log('uri', process.env.mongodb_uri);
+alert('uri', process.env.mongodb_uri);
 
 mongoose.connect(process.env.mongodb_uri, {
     useNewUrlParser: true,
@@ -14,7 +14,7 @@ mongoose.connect(process.env.mongodb_uri, {
 })
 
 mongoose.connection.on('connected', () => {
-    console.log('Connected!');
+    alert('Connected!');
 })
 
 const PORT = process.env.PORT || 3000
