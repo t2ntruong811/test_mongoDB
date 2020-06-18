dacf4ab22a4706cc2d97c795a6ec32e4b756292c
t2ntruong811 <t2ntruong811@gmail.com>
2020-06-18 21:33:21 +0700

    x

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.use((err, req, res, next) => {
     res.status(errStatus).send('View error log in console.');
 })
 
-const PORT = 3333;
+const PORT = 80;
 app.listen(PORT, _ => {
     console.log(`API is running at PORT: ${PORT}`);
 });
